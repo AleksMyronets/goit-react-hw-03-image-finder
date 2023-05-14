c90ba2d066d19872c2537375ba0907355a298e24
AleksMyronets <mironec.kiev.1993@gmail.com>
2023-05-14 17:13:49 +0300

    Сonnection сss components and jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,6 +5,7 @@ import { ImageGalleryItem } from './ImageGalleryItem';
 import { Loader } from './Loader';
 import { Modal } from './Modal';
 import { Saerchbar } from './Searchbar';
+import css from './App.module.css'
 
 
 
--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,4 +1,5 @@
 import PropTypes from 'prop-types';
+import css from './Button.module.css'
 
 export const Button = ({ onClick }) => {
     return <button type='button' onClick={onClick}>Lore more</button>
