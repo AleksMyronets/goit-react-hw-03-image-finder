c4254f01deab529a54f744ae51423e74ebbfcd8a
AleksMyronets <mironec.kiev.1993@gmail.com>
2023-05-16 12:31:57 +0300

    Update Loader.jsx

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -3,9 +3,9 @@ import css from './Loader.module.css'
 
 export const Loader = () => {
     return (<ThreeDots
-        height="100"
-        width="100"
-        radius="10"
+        height="85"
+        width="85"
+        radius="8"
         color="#4fa94d"
         ariaLabel="three-dots-loading"
         wrapperStyle={{}}
