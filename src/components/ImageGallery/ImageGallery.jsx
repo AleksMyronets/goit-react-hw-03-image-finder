94e444c9ffdc718fecf18b5508ccea68df9102b5
AleksMyronets <mironec.kiev.1993@gmail.com>
2023-05-16 14:24:00 +0300

    Update

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -2,36 +2,36 @@ import { Component } from 'react';
 import { ImageGalleryItem } from '../ImageGalleryItem';
 import { Modal } from '../Modal';
 import css from './ImageGallery.module.css';
+import PropTypes from 'prop-types';
 
-export class ImageGallery extends Component {
-  render() {
-    const { pictures, error, status, showeModal, urlPicture } =
-      this.props.state;
+export const ImageGallery = ({ pictures, onModal }) => {
+  return (
+    <>
+      {
+        <ul className={css.ImageGallery}>
+          {pictures &&
+            pictures.map(({ id, webformatURL, largeImageURL }) => (
+              <ImageGalleryItem
+                onClick={onModal}
+                key={id}
+                id={id}
+                picture={webformatURL}
+                largeImageURL={largeImageURL}
+              />
+            ))}
+        </ul>
+      }
+    </>
+  );
+};
 
-    if (status === 'rejected') {
-      return <h1>{error.message}</h1>;
-    }
-
-    return (
-      <>
-        {showeModal && (
-          <Modal onClose={this.props.onModal} urlPhoto={urlPicture} />
-        )}
-        {
-          <ul className={css.ImageGallery}>
-            {pictures &&
-              pictures.map(({ id, webformatURL, largeImageURL }) => (
-                <ImageGalleryItem
-                  onClick={this.props.onModal}
-                  key={id}
-                  id={id}
-                  picture={webformatURL}
-                  largeImageURL={largeImageURL}
-                />
-              ))}
-          </ul>
-        }
-      </>
-    );
-  }
-}
+ImageGallery.propTypes = {
+  onModal: PropTypes.func.isRequired,
+  pictures: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      webformatURL: PropTypes.string.isRequired,
+      largeImageURL: PropTypes.string.isRequired,
+    })
+  ),
+};
